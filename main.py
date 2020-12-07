c08412b1317e28e9a60e8051f38bbea8a2df7424
Janath Ranasinghe <janathmanthila95@gmail.com>
2020-12-07 14:28:29 +0530

    [IMP] add car selection functionality and log file identification

--- main.py
+++ main.py
@@ -7,11 +7,31 @@ from datetime import date, datetime, timedelta
 import time, os
 import decimal
 
+LOG_FILE_PATH = "/logs/"
 
 def float_range(start, stop, step):
     while start < stop:
         yield float(start)
         start += decimal.Decimal(step)
+
+
+class MsgBox(QDialog):
+
+    def __init__(self,  *args, title="", body=""):
+        super(MsgBox, self).__init__()
+
+        self.setWindowTitle(title)
+        body_txt = QLabel(body)
+
+        buttons = QDialogButtonBox.Ok
+
+        self.buttonBox = QDialogButtonBox(buttons)
+        self.buttonBox.accepted.connect(self.accept)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(body_txt)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
 
 
 class mainApplication(QWidget):
@@ -24,6 +44,9 @@ class mainApplication(QWidget):
         self.car_files = []
         self.open_file_dialog_btn = None
         self.time_range_cb = None
+        self.car_data = {}
+        self.related_logs = []
+        self.car_combo_box = None
 
         self.layoutMap = {}
         self.buttonMap = {}
@@ -65,6 +88,8 @@ class mainApplication(QWidget):
 
     #        self.show()
 
+    # LAYOUT RELATED FUNCTIONS
+
     def bottomLeft(self):
         self.bottomLeftBox = QGroupBox("First Graph")
 
@@ -78,7 +103,11 @@ class mainApplication(QWidget):
         # Create Layout
         leftLayout = QVBoxLayout()
         formLayout = QFormLayout()
-        formLayout.addRow('Car', QComboBox())
+
+        self.car_combo_box = QComboBox()
+        self.car_combo_box.currentIndexChanged.connect(lambda: self.get_log_files(self.car_combo_box.currentText()))
+        formLayout.addRow('Car', self.car_combo_box)
+
         leftLayout.addLayout(formLayout)
         leftLayout.addWidget(self.leftCanvas)
         # layout.addWidget(chooseButton)
@@ -207,11 +236,108 @@ class mainApplication(QWidget):
         buttons = QDialogButtonBox()
         buttons.setStandardButtons(
             QDialogButtonBox.Cancel | QDialogButtonBox.Apply)
+        # TODO: accepted not working. had to go with clicked. but work in both buttons. need to check
+        buttons.clicked.connect(self.initiate_analysis)
         leftLayout.addWidget(buttons)
         self.topLeftBox.setLayout(leftLayout)
 
     def topRight(self):
-        self.topRightBox = QTextEdit("")
+        layout = QVBoxLayout()
+        current_path = self.read_from_saved_data()
+        self.directory_path = QLineEdit(current_path if current_path else "")
+        self.directory_path.setReadOnly(True)
+        self.car_files_txt = QTextEdit("")
+        self.car_files_txt.setReadOnly(True)
+        layout.addWidget(QLabel("Current Path"))
+        layout.addWidget(self.directory_path)
+        layout.addWidget(QLabel("Selected CAR Files"))
+        layout.addWidget(self.car_files_txt)
+        self.topRightBox = QGroupBox()
+        self.topRightBox.setLayout(layout)
+
+    # LAYOUT RELATED FUNCTIONS :: END
+
+    # ANALYSIS RELATED FUNCTIONS
+
+    def clear_data(self):
+        """Clear data before analysis initiation"""
+        self.car_data = {}
+        self.related_logs = []
+
+    def initiate_analysis(self):
+        """Begin Analysis"""
+        self.clear_data()
+        print(self.directory_path.text())
+        print(self.car_files)
+        print(self.time_range_cb.currentText())
+        print(self.get_time_calculated())
+        # self.read_files()
+        self.get_car_data()
+        # update car combo box
+        self.car_combo_box.addItems([""])
+        self.car_combo_box.addItems([key for key, value in self.car_data.items()])
+        self.car_combo_box.update()
+
+    def get_car_data(self):
+        """Get cars' serial numbers as a list"""
+        for car_file_path in self.car_files:
+            car_file = open(car_file_path, 'r')
+            for line in car_file.readlines():
+                identifier = line.split(" ", 2)[0]
+                if not identifier == "0":
+                    self.car_data.update({
+                        identifier: line.split("\n")[0]
+                    })
+        if not self.car_data:
+            print("No car data found")
+            # TODO: this is not working
+            MsgBox(self, title="Error", body="No car data found")
+
+    def get_log_files(self, car_id):
+        """Find corresponding log files for given car"""
+        if car_id == "":
+            return
+        self.related_logs = []
+        car_data = self.car_data[car_id]
+        timestamp = car_data.split(" ")[2]
+        dt_object = self.get_time_calculated_from_timestamp(timestamp)
+        print( dt_object)
+        directory = self.read_from_saved_data() + LOG_FILE_PATH
+        for files in os.walk(directory):
+            for file in files[2]:
+                if ".log" in file:
+                    log_file = open(directory + '/' + file, 'r')
+                    lines = log_file.readlines()
+                    if len(lines) > 0:
+                        start_line_time = datetime.strptime(lines[0].split(": DEBUG")[0], "%Y-%b-%d %H:%M:%S.%f")
+                        end_line_time = datetime.strptime(lines[len(lines)-1].split(": DEBUG")[0], "%Y-%b-%d %H:%M:%S.%f")
+                        if start_line_time <= dt_object <= end_line_time:
+                            self.related_logs.append(directory + '/' + file)
+        if not self.related_logs:
+            print("No related log file(s) found")
+            # TODO: this is not working
+            MsgBox(self, title="Error", body="No related log file(s) found")
+
+    def get_time_calculated(self):
+        """Gets epoch time with related to given time range"""
+        current_date = datetime.today()
+        epoch = int(time.mktime(time.strptime(str(current_date), '%Y-%m-%d %H:%M:%S.%f')))  # current datetime to epoch
+        return self.get_time_calculated_from_timestamp(epoch)
+
+    def get_time_calculated_from_timestamp(self, timestamp):
+        """Gets epoch time from timestamp with related to given time range"""
+        after_converting = datetime.utcfromtimestamp(float(timestamp))  # converts epoch time to datetime
+        selected_time_range = self.time_range_cb.currentText()  # selected time range
+        selected_time_range_obj = datetime.strptime(selected_time_range[1:], '%H:%M')
+        if selected_time_range[0] == '-':
+            valid_time = after_converting - timedelta(hours=selected_time_range_obj.time().hour,
+                                                      minutes=selected_time_range_obj.time().minute)
+        else:
+            valid_time = after_converting + timedelta(hours=selected_time_range_obj.time().hour,
+                                                      minutes=selected_time_range_obj.time().minute)
+        return valid_time
+
+    # ANALYSIS INITIATION FUNCTIONS :: END
 
     def open_file_dialog_btn_click(self):
         path = QFileDialog.getExistingDirectory(None, 'Select Directory for files')
